65ba9a830b293a40434fa14c0e4c43bb8f9f96a9
Kelsey Nielsen <kelseywnielsen@gmail.com>
2021-05-02 16:43:07 -0600

    initial setup

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// start your server here
+const server = require("./api/server")
+
+const port = process.env.PORT || 4000
+
+server.listen(port, () => {
+    console.log(`Running at http://localhost:${port}`)
+})
